f8e6b08aa013bbdb0e50b9de33b9a7f3bcb215dd
Trọng Phúc Nguyễn <110124149@st.tvu.edu.vn>
2024-12-03 14:33:10 +0700

    Add files via upload

---
+++ 110124149.cpp
@@ -0,0 +1,74 @@
+#include<stdio.h>
+float a,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28,a29,a30;
+int main () 
+{
+	printf ("\nTinh tong chi tieu trong 30 ngay\n");
+	printf ("\nnhap chi tieu ngay 1: ");
+	scanf ("%f", &a);
+	printf ("nhap chi tieu ngay 2: ");
+	scanf ("%f", &a2);
+	printf ("nhap chi tieu ngay 3: ");
+	scanf ("%f", &a3);
+	printf ("nhap chi tieu ngay 4: ");
+	scanf ("%f", &a4);
+	printf ("nhap chi tieu ngay 5: ");
+	scanf ("%f", &a5);
+	printf ("nhap chi tieu ngay 6: ");
+	scanf ("%f", &a6);
+	printf ("nhap chi tieu ngay 7: ");
+	scanf ("%f", &a7);
+	printf ("nhap chi tieu ngay 8: ");
+	scanf ("%f", &a8);
+	printf ("nhap chi tieu ngay 9: ");
+	scanf ("%f", &a9);
+	printf ("nhap chi tieu ngay 10: ");
+	scanf ("%f", &a10);
+	printf ("nhap chi tieu ngay 11: ");
+	scanf ("%f", &a11);
+	printf ("nhap chi tieu ngay 12: ");
+	scanf ("%f", &a12);
+	printf ("nhap chi tieu ngay 13: ");
+	scanf ("%f", &a13);
+	printf ("nhap chi tieu ngay 14: ");
+	scanf ("%f", &a14);
+	printf ("nhap chi tieu ngay 15: ");
+	scanf ("%f", &a15);
+	printf ("nhap chi tieu ngay 16: ");
+	scanf ("%f", &a16);
+	printf ("nhap chi tieu ngay 17: ");
+	scanf ("%f", &a17);
+	printf ("nhap chi tieu ngay 18: ");
+	scanf ("%f", &a18);
+	printf ("nhap chi tieu ngay 19: ");
+	scanf ("%f", &a19);
+	printf ("nhap chi tieu ngay 20: ");
+	scanf ("%f", &a20);
+	printf ("nhap chi tieu ngay 21: ");
+	scanf ("%f", &a21);
+	printf ("nhap chi tieu ngay 22: ");
+	scanf ("%f", &a22);
+	printf ("nhap chi tieu ngay 23: ");
+	scanf ("%f", &a23);
+	printf ("nhap chi tieu ngay 24: ");
+	scanf ("%f", &a24);
+	printf ("nhap chi tieu ngay 25: ");
+	scanf ("%f", &a25);
+	printf ("nhap chi tieu ngay 26: ");
+	scanf ("%f", &a26);
+	printf ("nhap chi tieu ngay 27: ");
+	scanf ("%f", &a27);
+	printf ("nhap chi tieu ngay 28: ");
+	scanf ("%f", &a28);
+	printf ("nhap chi tieu ngay 29: ");
+	scanf ("%f", &a29);
+	printf ("nhap chi tieu ngay 30: ");
+	scanf ("%f", &a30);
+	float a31 = a + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10 +a11+a12+a13+a14+a15+a16+a17+a18+a19+a20+a21+a22+a23+a24+a25+a26+a27+a28+a29+a30;
+	if (a31,a,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28,a29,a30 >= 0 )
+	printf ("\ntong so chi tieu trong 30 ngay la: %f",a31);
+	if (a31,a,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28,a29,a30 < 0)
+	printf ("\nchi tieu khong hop le!");
+	
+		
+	return 0;
+}
